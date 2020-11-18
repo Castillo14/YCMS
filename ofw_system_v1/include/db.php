0741b1278c60ae9b18e63a47914bfff820846e92
Mary Joy Castillo <35132948+Castillo14@users.noreply.github.com>
2020-11-18 19:14:19 +0800

    db update

--- ofw_system_v1/include/db.php
+++ ofw_system_v1/include/db.php
@@ -1,12 +1,12 @@
 <?php
-	$con = mysqli_connect("localhost","urafnghd_ofw","","urafnghd_ofw");
+	$con = mysqli_connect("localhost","urafnghd_root","reniertrenuela9","urafnghd_ofw");
 
 	if(mysqli_connect_errno()){
 
 		echo "Failed to connect to MYSQL: " . mysqli_connect_error();
 		
 	}
-	$conn = mysqli_connect("localhost","urafnghd_polo","","urafnghd_polo");
+	$conn = mysqli_connect("localhost","urafnghd_root","reniertrenuela9","urafnghd_polo");
 
 	if(mysqli_connect_errno()){
 
